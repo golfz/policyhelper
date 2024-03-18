8e51ec5e6a435d3a6a8818babc90d4e513b2fca7
surattikorn <surattikorn@gmail.com>
2024-03-18 13:10:16 +0700

    feat: add comment for new ideas

--- policyhelper.go
+++ policyhelper.go
@@ -7,6 +7,14 @@ import (
 	"github.com/golfz/policyhelper/kpolicy"
 )
 
+// (1) this function should return []policy.Policy
+// ✅ or (2) return interface that can call IsAccessAllowed(policy.Resource) function
+//
+// I like the second option (2), because it's more flexible
+// and module's controller don't need to know the policy struct
+// and don't need to change the code if the policy struct is changed
+// and don't need to change the current code
+
 func GetPolicy(ctx goliath.Goliath) policy.Policy {
 	ctxPolicy := ctx.Request().Context().Value(kpolicy.UserPolicy)
 	p, ok := ctxPolicy.(policy.Policy)
